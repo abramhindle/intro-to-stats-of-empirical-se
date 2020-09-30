7eeaba7320a2368569d948ab7044fae73322f68f
Abram Hindle <abram.hindle@softwareprocess.es>
2020-09-30 12:21:17 -0600

    python-ols-demo

---
+++ Regression/ols.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+trueb = np.matrix([[3],[1],[2],[3]])
+ivars =  3
+n = 100
+X = np.ones((n,ivars+1))
+print("Generate some columns of independent variables")
+data = np.random.random((n,ivars))
+print("data.shape ",data.shape)
+X[:,1:(1+ivars)] = data
+print("X.shape ",X.shape)
+print("Now X has a column of ones and data in the rest")
+noise = 0.1 * np.random.random((n,1))
+print("calculate Y based on coeffecients of trueb and noise")
+Y = np.matmul(X,trueb) + noise
+print("Y.shape ", Y.shape)
+
+# What does it look like
+import seaborn
+import pandas
+dfX = pandas.DataFrame(X)
+dfX['y'] = Y
+# ry is rounded y (4 colours)
+dfX['ry'] = np.round(Y)
+seaborn.pairplot(dfX,hue='ry')
+plt.show()
+
+print("Now we want to OLS it!")
+print("(X' X)^-1 * (X' * Y)")
+Xt = np.transpose(X)
+B = np.matmul( np.linalg.inv(np.matmul(Xt,X)), np.matmul(Xt,Y))
+print("B.shape", B.shape)
+print("B: ", B)
+print("trueb - B", trueb - B)
+print("trueb - B", trueb - B)
+Yhat = np.matmul(X,B)
+def rms(Y1,Y2):
+   return np.sqrt(np.mean(np.array(Y1-Y2)** 2))
+print("rms(Y,Yhat)", rms(Y,Yhat))
+
+from sklearn.metrics import r2_score
+print("r2_score(Y,Yhat)", r2_score(Y,Yhat))
+
+
+
+dfX = pandas.DataFrame(X)
+dfX['y'] = Yhat
+# ry is rounded y (4 colours)
+dfX['ry'] = np.round(Y) # yes the original Y
+seaborn.pairplot(dfX,hue='ry')
+plt.show()
+
+
+
+Bnoise = B + 0.25*np.random.random(B.shape)
+print("Bnoise:",Bnoise)
+print("B - Bnoise", B - Bnoise)
+YhatNoise = np.matmul(X,Bnoise)
+print("rms(Y,YhatNoise)", rms(Y,YhatNoise))
+print("r2_score(Y,YhatNoise)", r2_score(Y,YhatNoise))
+
+
+dfX = pandas.DataFrame(X)
+dfX['y'] = YhatNoise
+# ry is rounded y (4 colours)
+dfX['ry'] = np.round(Y) # yes the original Y
+seaborn.pairplot(dfX,hue='ry')
+plt.show()
+
+# Ok lets compose this better
+
+def dataToX(data):
+    (n,ivars) = data.shape
+    X = np.ones((n,ivars+1))
+    X[:,1:(1+ivars)] = data
+    return X
+
+def fitOLS(data,Y):
+    X = dataToX(data)
+    Xt = np.transpose(X)
+    B = np.matmul( np.linalg.inv(np.matmul(Xt,X)), np.matmul(Xt,Y))
+    return B
+
+def predictOLS(data,B):
+    X = dataToX(data)
+    return np.matmul(X,B)    
+
+noise = 2 * np.random.random((n,1))
+print("calculate Y based on coeffecients of trueb and noise")
+data = np.random.random((n,ivars))
+X = dataToX(data)
+Y = np.matmul(X,trueb) + noise
+
+# Now we split 50/50
+Ytrain = Y[0:50,0]
+Ytest  = Y[50:,0]
+dataTrain = data[0:50,:]
+dataTest = data[50:,:]
+Btrain = fitOLS(dataTrain, Ytrain)
+Yhattrain = predictOLS(dataTrain,Btrain)
+print("rms(Ytrain,Yhattrain)", rms(Ytrain,Yhattrain))
+print("r2_score(Ytrain,Yhattrain)", r2_score(Ytrain,Yhattrain))
+
+# Now we test on the test set
+Yhattest = predictOLS(dataTest,Btrain)
+print("rms(Ytest,Yhattest)", rms(Ytest,Yhattest))
+print("r2_score(Ytest,Yhattest)", r2_score(Ytest,Yhattest))
+
+def effectOfNoise( noiseCoef ):
+    noise = noiseCoef * np.random.random((n,1))
+    trueb = np.random.random((ivars+1,1))
+    data = np.random.random((n,ivars))
+    X = dataToX(data)
+    Y = np.matmul(X,trueb) + noise
+    Ytrain = Y[0:50,0]
+    Ytest  = Y[50:,0]
+    dataTrain = data[0:50,:]
+    dataTest = data[50:,:]
+    Btrain = fitOLS(dataTrain, Ytrain)
+    Yhattrain = predictOLS(dataTrain,Btrain)
+    Yhattest = predictOLS(dataTest,Btrain)
+    return (r2_score(Ytrain,Yhattrain), r2_score(Ytest,Yhattest))
+
+for noiseCoef in [0.0, 0.1, 0.5, 1.0, 2.0, 4.0, 8.0]:
+    print(noiseCoef,
+          np.mean([effectOfNoise( noiseCoef) for i in range(40)],axis=0))
+
+# Lesson learned? Typically you do a lot worse on your test set
+# Train set performance != test set performance
+# More noise, harder to detect
--- stats-test.R
+++ stats-test.R
@@ -2,6 +2,23 @@ t.test(rnorm(100),rnorm(100))
 t.test(runif(100),rnorm(100))
 t.test(rnorm(100,1),rnorm(100))
 
+qq.
+
+t1 <- rnorm(50,0.7)
+t2 <- rnorm(50,0.0)
+t.test(t1,t2)
+ht1 <- hist(t1, plot=FALSE)
+ht2 <- hist(t2, plot=FALSE)
+
+# From Data Analytics UK
+# https://www.dataanalytics.org.uk/plot-two-overlapping-histograms-on-one-chart-in-r/
+c1 <- rgb(173,216,255, max = 255, alpha = 80, names = "lt.blue")
+c2 <- rgb(255,255,203, max = 255, alpha = 80, names = "lt.yellow")
+par(mfrow=c(2,1))
+plot(ht1,col=c1)
+plot(ht2,col=c2)
+par(mfrow=c(1,1))
+
 
 wilcox.test(rnorm(100),rnorm(100))
 wilcox.test(runif(100),rnorm(100))
@@ -54,19 +71,25 @@ chisq.test(st3)
 
 # boot strap mean
 
-N=10000
+N=1000
+REPS=10000
 alpha = 0.05
-data = rnorm(N)
+data = rnorm(N,0.0) + rnorm(N,1.0,0.1)
+plot(density(data))
 mean(data)
 mean(sample(data,N,replace=TRUE))
-booted <- sapply(c(1:N), function(i) { mean(sample(data,N,replace=TRUE)) })
+booted <- sapply(c(1:REPS), function(i) { mean(sample(data,N,replace=TRUE)) })
 mean(booted)
 summary(booted)
 quantile(booted,c(alpha/2,1.0 - alpha/2))
+par(mfrow=c(2,1))
 plot(density(booted))
+plot(density(data))
+par(mfrow=c(1,1))
 
 # difference of means
 
+REPS=1000
 N=100
 alpha = 0.05
 data1 = rnorm(N)
@@ -74,25 +97,69 @@ data2 = rnorm(N,mean=0.5)
 mean(data1)
 mean(data2)
 mean(data1) -  mean(data2)    
-booted <- sapply(c(1:N*100), function(i) { mean( sample(data1,N,replace=TRUE) ) - mean( sample(data2,N,replace=TRUE)  ) })
+booted <- sapply(c(1:REPS), function(i) {
+    mean( sample(data1,N,replace=TRUE) ) -
+        mean( sample(data2,N,replace=TRUE)  )
+})
+booted <- sapply(c(1:REPS), function(i) {
+    t.test(sample(data1,N,replace=TRUE) ,
+           sample(data2,N,replace=TRUE) )$p.value
+})
 mean(booted)
 summary(booted)
 quantile(booted,c(alpha/2,1.0-alpha/2))
+par(mfrow=c(3,1))
 plot(density(booted))
+plot(density(data1))
+plot(density(data2))
 
-counts <- c(100,100,100,500,500,500,1000,1000,10000)
-boots <- sapply(counts, function(N) {
+# Demonstrate difference with sample size
+par(mfrow=c(1,1))
+counts <- c(100,500,1000,10000)
+REPS=1000
+boots <- lapply(counts, function(N) {
     alpha = 0.05
     data1 = rnorm(N)
     data2 = rnorm(N,mean=0.5)
-    data2[1] = 10
+    data2[1] = 100 # add an outlier
     mean(data1)
     mean(data2)
     mean(data1) -  mean(data2)    
-    booted <- sapply(c(1:N), function(i) { mean( sample(data1,N,replace=TRUE) ) - mean( sample(data2,N,replace=TRUE)  ) })
+    booted <- sapply(c(1:REPS), function(i) {
+        mean( sample(data1,N,replace=TRUE) ) -
+            mean( sample(data2,N,replace=TRUE)  ) })
     print(mean(booted))
     print(summary(booted))
     print(N)
+    print(REPS)
+    print(quantile(booted,c(alpha/2,1.0 - alpha/2)))
+    booted
+})
+# boots
+plot(density(boots[[length(boots)]]),xlim=c(-1,0.25),ylim=c(0,30))
+for (i in c(1:length(boots))) {
+    lines(density(boots[[i]]),col=i)
+}
+legend(0,30,counts,pch=1,col=c(1:length(boots)))
+
+
+# Demonstrate difference with repetitions
+par(mfrow=c(1,1))
+rcount <- 100
+data1 = rnorm(rcount)
+data2 = rnorm(rcount,mean=0.5)
+data2[1] = 100
+REPS <- c(10,100,1000,10000,100000)
+boots <- lapply(REPS, function(N) {
+    alpha = 0.05
+    mean(data1)
+    mean(data2)
+    mean(data1) -  mean(data2)    
+    booted <- sapply(c(1:N), function(i) { mean( sample(data1,rcount,replace=TRUE) ) - mean( sample(data2,rcount,replace=TRUE)  ) })
+    print(mean(booted))
+    print(summary(booted))
+    print(N)
+    print(REPS)
     print(quantile(booted,c(alpha/2,1.0 - alpha/2)))
     booted
 })
@@ -100,20 +167,27 @@ plot(density(boots[[length(boots)]]),xlim=c(-1,0.25),ylim=c(0,30))
 for (i in c(1:length(boots))) {
     lines(density(boots[[i]]),col=i)
 }
-legend(0,30,counts,pch=1,col=c(1:length(boots)))
+legend(0,30,REPS,pch=1,col=c(1:length(boots)))
+
+
+
+
 
 # difference of skews
 # install.packages("moments")
 library(moments)
-    N=100
-    alpha = 0.05
-    data1 = rnorm(N)
-    data2 = rnorm(N,mean=0.5)
-    median(data1)
-    median(data2)
-    median(data1) -  median(data2)    
-    booted <- sapply(c(1:N), function(i) { median( sample(data1,N,replace=TRUE) ) - median( sample(data2,N,replace=TRUE)  ) })
-    mean(booted)
-    summary(booted)
-    quantile(booted,c(alpha/2,1.0 - alpha/2))
-    plot(density(booted))
+N=100
+REPS=1000
+alpha = 0.05
+data1 = rnorm(N)
+data2 = rnorm(N,mean=0.5)
+skewness(data1)
+skewness(data2)
+skewness(data1) -  skewness(data2)    
+booted <- sapply(c(1:REPS), function(i) {
+    skewness( sample(data1,N,replace=TRUE) ) - skewness( sample(data2,N,replace=TRUE)  )
+})
+mean(booted)
+summary(booted)
+quantile(booted,c(alpha/2,1.0 - alpha/2))
+plot(density(booted))
